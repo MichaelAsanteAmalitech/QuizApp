f040cd548ec7f2dfee5b2360c9557f5298736133
MichaelAsamteAmalitech <michael.asante@amalitech.org>
2020-10-10 20:00:33 +0000

    Added some styling to the custom.css and structured the questions for the quiz App

---
+++ js/questions.js
@@ -0,0 +1,36 @@
+// creating an array and passing the number, questions, options, and answers to it
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following is not a Java feature?",
+    answer: "Use of Pointers",
+    options: [
+      "Dynamic",
+      "Use of Pointers",
+      "Architecture Neutral",
+      "Object-Oriented"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is used to fix bugs in Java programs?",
+    answer: "JDB",
+    options: [
+      "JVM",
+      "JRE",
+      "JDK",
+      "JDB"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the return type of the hashCode() method on the Object class?",
+    answer: "int",
+    options: [
+      "Object",
+      "int",
+      "long",
+      "void"
+    ]
+  }
+];
