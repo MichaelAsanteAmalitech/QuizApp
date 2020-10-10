0ea353df6cf1ac97d76b449c3cd1e9443858c30b
MichaelAsamteAmalitech <michael.asante@amalitech.org>
2020-10-10 21:32:02 +0000

    The final working quiz app with a video index.html which leads to where you will begin your quiz

--- js/questions.js
+++ js/questions.js
@@ -1,36 +1,72 @@
-// creating an array and passing the number, questions, options, and answers to it
+// creating an array and passing the number, questions, options, and answers
 let questions = [
     {
     numb: 1,
-    question: "Which of the following is not a Java feature?",
-    answer: "Use of Pointers",
+    question: "Multiple inheritance means, ___?",
+    answer: "one class inheriting from more super classes",
     options: [
-      "Dynamic",
-      "Use of Pointers",
-      "Architecture Neutral",
-      "Object-Oriented"
+      "one class inheriting from more super classes",
+      "more classes inheriting from one super class",
+      "more classes inheriting from more super classes",
+      "None of the above"
     ]
   },
     {
     numb: 2,
-    question: "What is used to fix bugs in Java programs?",
-    answer: "JDB",
+    question: " What is JRE?",
+    answer: "JRE is an implementation of the Java Virtual Machine which executes Java programs.",
     options: [
-      "JVM",
-      "JRE",
-      "JDK",
-      "JDB"
+      "JRE is a java based GUI application.",
+      "JRE is an application development framework.",
+      "JRE is an implementation of the Java Virtual Machine which executes Java programs.",
+      "None of the above."
     ]
   },
     {
     numb: 3,
-    question: "What is the return type of the hashCode() method on the Object class?",
-    answer: "int",
+    question: "This is the parent of Error and Exception classes?",
+    answer: "Throwable",
     options: [
-      "Object",
-      "int",
-      "long",
-      "void"
+      "Throwable",
+      "Catchable",
+      "MainError",
+      "MainException"
     ]
-  }
+  },
+    {
+    numb: 4,
+    question: "In which case, a program is not expected to recover?",
+    answer: "If an error occurs.",
+    options: [
+      "If an error occurs.",
+      "If an exception occurs.",
+      "Both of the above.",
+      "None of the above."
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which arithmetic operations can result in the throwing of an ArithmeticException?",
+    answer: "/ , %",
+    options: [
+      "/ , %",
+      "* , +",
+      " ! , -",
+      ">>, <<"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
 ];
